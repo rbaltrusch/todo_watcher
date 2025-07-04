4721baf9e1d8022d642da09808186edf08c1ae2e
Richard Baltrusch <richard@baltrusch.net>
2025-07-04 14:05:32 +0200

    Split up filewatcher

--- filewatcher/filewatcher.go
+++ filewatcher/filewatcher.go
@@ -1,48 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/fsnotify/fsnotify"
 )
 
-var WATCH_PATH = "C:/Users/richa/Desktop/todo"
+func handleFileEvents(watcher *fsnotify.Watcher) {
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			log.Println("event:", event)
+			if event.Has(fsnotify.Write) {
+				log.Println("modified file:", event.Name)
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			log.Println("error:", err)
+		}
+	}
+}
 
-func main() {
+func createWatcher(path string) *fsnotify.Watcher {
 	// Create new watcher.
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer watcher.Close()
 
-	// Start listening for events.
-	go func() {
-		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return
-				}
-				log.Println("event:", event)
-				if event.Has(fsnotify.Write) {
-					log.Println("modified file:", event.Name)
-				}
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return
-				}
-				log.Println("error:", err)
-			}
-		}
-	}()
-
-	// Add a path.
-	err = watcher.Add(WATCH_PATH)
+	// Add a path to watch.
+	err = watcher.Add(path)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return watcher
+}
 
-	// Block main goroutine forever.
-	<-make(chan struct{})
+func main() {
+	fmt.Println("Starting file watcher...")
+	path := "C:/Users/richa/Desktop/todo"
+	watcher := createWatcher(path)
+	defer watcher.Close()
+	go handleFileEvents(watcher)
+	select {} // Block main goroutine forever.
 }
--- filewatcher/go.mod
+++ filewatcher/go.mod
@@ -3,6 +3,6 @@ module filewatcher
 go 1.19
 
 require (
-	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/fsnotify/fsnotify v1.9.0
 	golang.org/x/sys v0.13.0 // indirect
 )
